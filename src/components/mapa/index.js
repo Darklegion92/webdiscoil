a807fffdb2a258fe098b01cc30f191c94009e520
JOSE LUIS RODRIGUEZ PEÑARANDA <joserodriguez@saludfamiliar.com.co>
2020-11-25 13:11:04 -0500

    nada

--- src/components/mapa/index.js
+++ src/components/mapa/index.js
@@ -1,54 +1,54 @@
-import React, { Component } from 'react'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
-import L from 'leaflet'
-import { WrapperConsumer } from '../../store'
+import React, { Component } from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import { WrapperConsumer } from "../../store";
 
 const styles = {
   wrapper: {
     height: 550,
-    width: '100%',
-    margin: '0 auto',
-    display: 'flex'
+    width: "100%",
+    margin: "0 auto",
+    display: "flex",
   },
   map: {
-    flex: 1
-  }
-}
+    flex: 1,
+  },
+};
 class Mapa extends Component {
-  shouldComponentUpdate (nextProps, nextState) {
-    return this.props.context.punteros !== nextProps.context.punteros
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.context.punteros !== nextProps.context.punteros;
   }
 
   myIcon = L.icon({
-    iconUrl: 'loc.png',
+    iconUrl: "loc.png",
     iconSize: [30, 30],
     iconAnchor: [10, 10],
     popupAnchor: [-1, -10],
-    shadowUrl: 'my-icon-shadow.png',
+    shadowUrl: "my-icon-shadow.png",
     shadowSize: [0, 0],
-    shadowAnchor: [10, 10]
-  })
+    shadowAnchor: [10, 10],
+  });
 
-  render () {
-    const { punteros } = this.props.context
-    const { market } = this.props
-    console.log(punteros)
+  render() {
+    const { punteros } = this.props.context;
+    const { market } = this.props;
+    console.log(punteros);
     return (
       <div style={styles.wrapper}>
         <Map
           center={[7.869940353376568, -72.50701904296875]}
           zoom={12}
           style={styles.map}
-          id='mapID'
+          id="mapID"
         >
           <TileLayer
-            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
           {!market &&
             punteros.length > 0 &&
-            punteros.map(puntero => {
-              const fecha = new Date(puntero.fecha)
+            punteros.map((puntero) => {
+              const fecha = new Date(puntero.fecha);
               return (
                 <Marker
                   position={[puntero.latitude, puntero.longitude]}
@@ -64,12 +64,12 @@ class Mapa extends Component {
                     {puntero.tipo}
                   </Popup>
                 </Marker>
-              )
+              );
             })}
           {market && <Marker position={market} icon={this.myIcon}></Marker>}
         </Map>
       </div>
-    )
+    );
   }
 }
-export default WrapperConsumer(Mapa)
+export default WrapperConsumer(Mapa);
--- src/config/CONSTANTES.js
+++ src/config/CONSTANTES.js
@@ -1,5 +1,5 @@
 const CONSTANTES = {
-  APIREST: 'http://localhost:8085',
+  APIREST: 'http://45.82.72.196:8085',
   EXTENSION: '.webp'
 }
 
